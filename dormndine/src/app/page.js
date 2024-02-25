744dfbce2c36a06e505adc2fe05269bdeebfb3c6
Muskaan Joshi <jmuskaan@unc.edu>
2024-02-24 19:26:43 -0500

    home page dev

--- dormndine/src/app/page.js
+++ dormndine/src/app/page.js
@@ -3,93 +3,52 @@ import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Dorm-N-Dine
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
+    <div>
+      <img id="logo" src="/src/app/img/DormNDineLogo.png"></img>
+      <body className={styles.main}>
+        <h1>Dorm-N-Dine</h1>
+       
+        <form class="form">
+          <div class="search-bar">
+            <input class="query" type="search" placeholder="Search All Recipes..." style={{ width: '400px', height: '40px', }} />
+            <button class="bar">Search</button>
+          </div>
+          <br></br>
+          <br></br>
+          <h2>Or</h2>
+          <br></br>
+          <br></br>
+          <label htmlFor="meal-type">Meal Type:</label>
+          <select id="meal-type" name="meal-type" className={styles.select}>
+            <option value="breakfast">Breakfast</option>
+            <option value="lunch">Lunch</option>
+            <option value="dinner">Dinner</option>
+            <option value="snack">Snack</option>
+          </select>
+          
+          <label htmlFor="ingredients">ingredients:</label>
+          <select id="ingredients" name="ingredients" className={styles.select}>
+            <option value="apples">apples</option>
+            <option value="bananas">bananas</option>
+            <option value="carrots">cheese</option>
+            <option value="lettuce">lettuce</option>
+          </select>
+          
+          <label htmlFor="Restrictions">Restrictions:</label>
+          <select id="Restrictions" name="Restrictions" className={styles.select}>
+            <option value="Vegan">Vegan</option>
+            <option value="Vegetarian">Vegetarian</option>
+            <option value="Gluten Free">Gluten Free</option>
+            <option value="Dairy-free">Dairy-free</option>
+          </select>
+          <br></br>
+          <br></br>
+          <a href="/new-page">
+            <button type="submit">Search</button>
           </a>
-        </div>
-      </div>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+        </form>
+      </body>
+    </div>
   );
 }
